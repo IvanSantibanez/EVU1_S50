fe57d92ff1da3338c2121222943abfc9ebd04b43
Iván Santibáñez <iasantibanez@indracompany.com>
2025-08-16 13:23:12 -0400

    Se crea migración para Proyectos, relación entre usuario y proyectos, y seeder para los datos de pruebas

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -3,15 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Proyecto extends Model
 {
     protected $fillable = [
-        'id',
         'nombre',
         'fechaInicio',
         'estado',
         'responsable',
         'monto',
+        'created_by'
     ];
+
+     public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
