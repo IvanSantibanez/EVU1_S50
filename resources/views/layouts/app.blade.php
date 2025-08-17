948e47938cb5303690f7aa401df0422c6498da27
Iván Santibáñez <iasantibanez@indracompany.com>
2025-08-17 12:53:11 -0400

    Se agrega vistapara registro y login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
@@ -23,14 +24,9 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Proyectos</a>
+                        <a class="nav-link" href="/proyectos">Proyectos</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contacto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Acerca de</a>
-                    </li>
+                    <template id="nav-auth-links"></template>
                 </ul>
             </div>
         </div>
@@ -49,6 +45,53 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js" integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous"></script>
     <script src="{{ asset('assets/js/uf.js') }}"></script>
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        function updateNavAuth() {
+            const template = document.getElementById('nav-auth-links');
+            const parent = document.querySelector('.navbar-nav');
+            if (!template || !parent) return;
+
+            parent.querySelectorAll('.injected-auth').forEach(el => el.remove());
+
+            const token = localStorage.getItem('token');
+            if (token) {
+                const html = `\
+                    <li class="nav-item dropdown injected-auth">\
+                        <a class="nav-link dropdown-toggle" href="#" id="userMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cuenta</a>\
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userMenu">\
+                            <li><a class="dropdown-item" href="/proyectos">Mis proyectos</a></li>\
+                            <li><a class="dropdown-item" href="#" id="logoutBtn">Cerrar sesión</a></li>\
+                        </ul>\
+                    </li>`;
+                parent.insertAdjacentHTML('beforeend', html);
+
+                const logout = document.getElementById('logoutBtn');
+                if (logout) {
+                    logout.addEventListener('click', function (e) {
+                        e.preventDefault();
+                        localStorage.removeItem('token');
+                        updateNavAuth();
+                        window.location.href = '/';
+                    });
+                }
+            } else {
+                const html = `\
+                    <li class="nav-item d-flex align-items-center injected-auth">\
+                        <a class="nav-link" href="/login">Iniciar Sesión</a>\
+                    </li>\
+                    <li class="nav-item d-flex align-items-center injected-auth">\
+                        <a class="btn btn-outline-light btn-sm ms-2" href="/register">Registrarse</a>\
+                    </li>`;
+                parent.insertAdjacentHTML('beforeend', html);
+            }
+        }
+
+        window.updateNavAuth = updateNavAuth;
+        updateNavAuth();
+    });
+    </script>
 
 </body>
 
